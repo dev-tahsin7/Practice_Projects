29d3e4a6752f6b9b89ab071fba59c146d7fd3a15
dev-tahsin7 <tahsinahmad2004@gmail.com>
2023-10-14 20:00:06 +0600

    New Dice ROlling project

---
+++ dice_rolling.py
@@ -0,0 +1,13 @@
+import random
+
+dice = True
+while dice:
+    print(random.randint(1,6))
+    again = input("Enter Yes or No like y/n: ")
+    if again == "y":
+        continue
+    else:
+        break
+
+# Dice Rolling Game in just 10 line .. 
+# জানি এটা একটা ফকিরা মার্কা প্রজেক্ট কিন্তু নতুন হিসাবে এটাই অনেক ! 
